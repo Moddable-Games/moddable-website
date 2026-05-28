fd9771ebdd5672164309d356e372618bc715847c
Mark Smalley <mark@smalley.my>
2026-05-28 13:05:17 +0100

    Bump version to 1.0.43

--- js/mg-loader.js
+++ js/mg-loader.js
@@ -4,7 +4,7 @@
    ========================================================================= */
 
 (function() {
-  var V = '1.0.42';
+  var V = '1.0.43';
   const scripts = document.querySelectorAll('script[src*="mg-loader.js"]');
   const src = scripts[scripts.length - 1].getAttribute('src');
   const base = src.replace(/mg-loader\.js.*$/, '');
