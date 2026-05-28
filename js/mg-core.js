fad1247e63dfa3f578cd37e795511f5e9447af55
Mark Smalley <mark@smalley.my>
2026-05-28 13:08:22 +0100

    Bump version to 1.0.44

--- js/mg-core.js
+++ js/mg-core.js
@@ -4,7 +4,7 @@
    ========================================================================= */
 
 window.MG = (() => {
-  const VERSION = '1.0.43';
+  const VERSION = '1.0.44';
   const META_BASE = (document.querySelector('meta[name="mg-base"]') || {}).content;
   const BASE = META_BASE != null ? META_BASE
     : location.pathname.includes('/MODDABLE/moddable-website')
