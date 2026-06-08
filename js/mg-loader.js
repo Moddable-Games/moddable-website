4ef4df08fac37e3b1eda1692269b642ab56cb08a
Mark Smalley <mark@smalley.my>
2026-06-08 20:39:06 +0100

    Fix stats spacing: add gap to both stats-row and stats-bar__inner on mobile, hide dividers

--- js/mg-loader.js
+++ js/mg-loader.js
@@ -4,7 +4,7 @@
    ========================================================================= */
 
 (function() {
-  var V = '1.0.96';
+  var V = '1.0.97';
   const scripts = document.querySelectorAll('script[src*="mg-loader.js"]');
   const src = scripts[scripts.length - 1].getAttribute('src');
   const base = src.replace(/mg-loader\.js.*$/, '');
