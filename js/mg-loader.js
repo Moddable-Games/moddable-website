0bffa2e57b2b4382ce5542afd7b656f219a906fa
Mark Smalley <mark@smalley.my>
2026-06-08 20:52:35 +0100

    Prose lists: left-align with text-wrap:balance instead of inheriting justify

--- js/mg-loader.js
+++ js/mg-loader.js
@@ -4,7 +4,7 @@
    ========================================================================= */
 
 (function() {
-  var V = '1.0.103';
+  var V = '1.0.104';
   const scripts = document.querySelectorAll('script[src*="mg-loader.js"]');
   const src = scripts[scripts.length - 1].getAttribute('src');
   const base = src.replace(/mg-loader\.js.*$/, '');
