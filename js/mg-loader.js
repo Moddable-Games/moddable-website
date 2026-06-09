affcc8a227b0250cf5db71c76a5551bf12ab8287
Mark Smalley <mark@smalley.my>
2026-06-09 09:09:37 +0100

    Fix 1px black line between hero and gallery on iOS (sub-pixel scroll gap)

--- js/mg-loader.js
+++ js/mg-loader.js
@@ -4,7 +4,7 @@
    ========================================================================= */
 
 (function() {
-  var V = '1.0.131';
+  var V = '1.0.132';
   const scripts = document.querySelectorAll('script[src*="mg-loader.js"]');
   const src = scripts[scripts.length - 1].getAttribute('src');
   const base = src.replace(/mg-loader\.js.*$/, '');
